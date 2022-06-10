dc3582b6bdc6208cb32fe1f294ba7cd443701127
Lautaro Chaar <lautarochaar@hotmail.com>
2022-06-10 18:36:54 -0300

    Agregando arrays

--- game.js
+++ game.js
@@ -1,17 +1,18 @@
 let dificulty = Number(prompt("Seleccione en que dificultad desea jugar: 1, 2 o 3"));
-const createCards = (i) => console.log(`Carta ${i}`);
+let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+
+const createCards = (x, y) => {
+    let gameDifficulty = numbers.slice(x, y);
+    console.log(gameDifficulty.sort(()=>Math.random() - 0.5));
+}
 
 if (dificulty === 1) {
-    for (let i = 0; i < 8; i++){
-        createCards(i);
-    }
+        createCards(0,8);
 } else if (dificulty === 2) {
-    for (let i = 0; i < 14 ; i++){
-        createCards(i);
-    }
+        createCards(0,14);
 } else if (dificulty === 3) {
-    for (let i = 0; i < 20; i++)
-        createCards(i);
+        createCards();
     } else {
         alert("No fue posible establecer la dificultad.");
     }
