f8896dacf689b94b7f3aaa1438ab489cd75c6eed
Lautaro Chaar <lautarochaar@hotmail.com>
2022-06-15 20:08:20 -0300

    Agregando array methods

--- game.js
+++ game.js
@@ -1,19 +1,24 @@
 let dificulty = Number(prompt("Seleccione en que dificultad desea jugar: 1, 2 o 3"));
 let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
-
-const createCards = (x, y) => {
-    let gameDifficulty = numbers.slice(x, y);
-    console.log(gameDifficulty.sort(()=>Math.random() - 0.5));
+// Generamos el juego en base a su dificultad
+const rangeOfNumbers = (x,y) => {
+    let totalNumbers = numbers.slice(x, y);
+    totalNumbers.sort(()=>Math.random() - 0.5);
+    totalNumbers.forEach ((number) => {
+        console.log(number);
+    })
 }
 
+
 if (dificulty === 1) {
-        createCards(0,8);
+    rangeOfNumbers(0,8);
 } else if (dificulty === 2) {
-        createCards(0,14);
+    rangeOfNumbers(0,14);
 } else if (dificulty === 3) {
-        createCards();
+    rangeOfNumbers();
     } else {
         alert("No fue posible establecer la dificultad.");
     }
+
 
