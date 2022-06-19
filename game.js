a999c6e2142c3bd367bc1679b88f79bb7a5686d6
Lautaro Chaar <lautarochaar@hotmail.com>
2022-06-19 15:33:32 -0300

    Objetos

--- game.js
+++ game.js
@@ -1,5 +1,13 @@
+let playerName = prompt("Ingrese su nombre:");
 let dificulty = Number(prompt("Seleccione en que dificultad desea jugar: 1, 2 o 3"));
+
 let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+let player = {
+    name: playerName,
+    moves: 'moves',
+    time: 'time'
+}
+
 
 // Generamos el juego en base a su dificultad
 const rangeOfNumbers = (x,y) => {
