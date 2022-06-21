dbd1665209ad205619ec1e35a58786eef46169f1
Lautaro Chaar <lautarochaar@hotmail.com>
2022-06-21 09:32:27 -0300

    Manipulando el DOM

--- game.js
+++ game.js
@@ -1,3 +1,9 @@
+const startButton = document.getElementById("startButton");
+const restartButton = document.getElementById("restartButton");
+const selection = document.getElementById("dificultad");
+const buttonContainer = document.getElementById("game");
+const moves = document.getElementById("moves");
+const showTime = document.getElementById("time");
 let playerName = prompt("Ingrese su nombre:");
 let dificulty = Number(prompt("Seleccione en que dificultad desea jugar: 1, 2 o 3"));
 
@@ -12,9 +18,10 @@ let player = {
 // Generamos el juego en base a su dificultad
 const rangeOfNumbers = (x,y) => {
     let totalNumbers = numbers.slice(x, y);
-    totalNumbers.sort(()=>Math.random() - 0.5);
-    totalNumbers.forEach ((number) => {
-        console.log(number);
+    totalNumbers.forEach (() => {
+        let button = document.createElement ("BUTTON");
+        button.setAttribute("class", "btnGame")
+        buttonContainer.appendChild(button);
     })
 }
 
